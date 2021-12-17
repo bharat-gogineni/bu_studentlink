5b22b5ebb8a02a9d0cceb4feb2cacb299a2e26be
Bharat <bharatgoku@gmail.com>
2021-12-16 21:33:36 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { IntlProvider } from 'react-intl';
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import Layout from './layout/Layout';
+import messages from './messages';
+import './styles/App.scss';
+import CircularProgress from '@mui/material/CircularProgress';
+
+  class App extends Component {
+    render() {
+      return (
+    <HashRouter>
+    <React.Suspense fallback={<CircularProgress />}>
+      <Routes>
+        <Route path="/*" name="Home" element={<IntlProvider locale='en' messages={messages['en']}><Layout setLocale='en' /></IntlProvider>} />
+      </Routes>
+    </React.Suspense>
+  </HashRouter>
+  );
+  }
+}
+
+export default App
